69b01f96b7b4903b52f48767ec5bb5d74ec4ce25
corganfuzz <gruizcavero@uh.edu>
2020-05-30 15:30:16 -0500

    changed readme

--- src/components/users/UserItem.js
+++ src/components/users/UserItem.js
@@ -1,13 +1,9 @@
-import React, { Component } from 'react'
+import React, { Component } from "react";
 
 class UserItem extends Component {
-    render() {
-        return (
-            <div>
-                
-            </div>
-        )
-    }
+  render() {
+    return <div></div>;
+  }
 }
 
-export default UserItem
+export default UserItem;
