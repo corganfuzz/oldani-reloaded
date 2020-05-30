0c366551c4b9f04e12c21c32313cb54aa3b9835a
corganfuzz <gruizcavero@uh.edu>
2020-05-30 16:19:11 -0500

    hardcoded data

--- src/components/users/UserItem.js
+++ src/components/users/UserItem.js
@@ -1,8 +1,44 @@
 import React, { Component } from "react";
 
 class UserItem extends Component {
+  //   state = {
+  //     id: "id",
+  //     login: "mojombo",
+  //     avatar_url: "https://avatars0.githubusercontent.com/u/1?v=4",
+  //     html_url: "https://github.com/mojombo",
+  //   };
+
+  // A constructor is basically a function that runs when the component starts
+  // THE OLD WAY=====>
+  //   constructor() {
+  //     super();
+  //     this.state = {
+  //       id: "id",
+  //       login: "mojombo",
+  //       avatar_url: "https://avatars0.githubusercontent.com/u/1?v=4",
+  //       html_url: "https://github.com/mojombo",
+  //     };
+  //   }
+  // END OF THE OLD WAY
+
   render() {
-    return <div></div>;
+    const { login, avatar_url, html_url } = this.props.user;
+    return (
+      <div className="card text-center">
+        <img
+          src={avatar_url}
+          alt=""
+          className="round-img"
+          style={{ width: "60px" }}
+        />
+        <h3>{login}</h3>
+        <div>
+          <a href={html_url} className="btn btn-dark btn-sm my-1">
+            More
+          </a>
+        </div>
+      </div>
+    );
   }
 }
 
