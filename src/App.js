b2bf58350a9e0f20b8cefab5db3e269f415f24d4
corganfuzz <gruizcavero@uh.edu>
2020-06-01 18:20:10 -0500

    search component working

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import "./App.css";
 import Navbar from "./components/layout/Navbar";
 import Users from "./components/users/Users";
 import axios from "axios";
+import Search from "./components/users/Search";
 
 class App extends Component {
   state = {
@@ -10,23 +11,35 @@ class App extends Component {
     loading: false,
   };
 
-  async componentDidMount() {
-    this.setState({ loading: true });
-    // OLD WAY
-    // axios
-    //   .get("https://api.github.com/users")
-    //   .then((res) => console.log(res.data));
+  // async componentDidMount() {
+  //   // console.log("bro", process.env.REACT_APP_GITHUB_CLIENT_SECRET);
+  //   this.setState({ loading: true });
+  //   // OLD WAY
+  //   // axios
+  //   //   .get("https://api.github.com/users")
+  //   //   .then((res) => console.log(res.data));
 
-    // NEW WAY added async to componentDidMount
-    const res = await axios.get("https://api.github.com/users");
-    this.setState({ users: res.data, loading: false });
-  }
+  //   // NEW WAY added async to componentDidMount
+  //   const res = await axios.get(`https://api.github.com/users?client_id=$
+  //   {process.env.REACT_APP_GITHUB_CLIENT_ID}&client_secret=$
+  //   {process.env.REACT_APP_GITHUB_CLIENT_SECRET}`);
+  //   this.setState({ users: res.data, loading: false });
+  // }
+
+  // Search Github Users
+  searchUsers = async (text) => {
+    const res = await axios.get(`https://api.github.com/search/users?q=${text}&client_id=$
+    {process.env.REACT_APP_GITHUB_CLIENT_ID}&client_secret=$
+    {process.env.REACT_APP_GITHUB_CLIENT_SECRET}`);
+    this.setState({ users: res.data.items, loading: false });
+  };
 
   render() {
     return (
       <div className="App">
         <Navbar />
         <div className="container">
+          <Search searchUsers={this.searchUsers} />
           <Users loading={this.state.loading} users={this.state.users} />
         </div>
       </div>
