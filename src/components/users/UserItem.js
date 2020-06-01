fd633b7fc73fdde9ecb7da149208c6640706403d
corganfuzz <gruizcavero@uh.edu>
2020-06-01 14:45:15 -0500

    getting rid of state

--- src/components/users/UserItem.js
+++ src/components/users/UserItem.js
@@ -1,6 +1,8 @@
-import React, { Component } from "react";
+import React from "react";
+// import React, { Component } from "react";
 
-class UserItem extends Component {
+const UserItem = (props) => {
+  // class UserItem extends Component {
   //   state = {
   //     id: "id",
   //     login: "mojombo",
@@ -21,25 +23,23 @@ class UserItem extends Component {
   //   }
   // END OF THE OLD WAY
 
-  render() {
-    const { login, avatar_url, html_url } = this.props.user;
-    return (
-      <div className="card text-center">
-        <img
-          src={avatar_url}
-          alt=""
-          className="round-img"
-          style={{ width: "60px" }}
-        />
-        <h3>{login}</h3>
-        <div>
-          <a href={html_url} className="btn btn-dark btn-sm my-1">
-            More
-          </a>
-        </div>
+  const { login, avatar_url, html_url } = props.user;
+  return (
+    <div className="card text-center">
+      <img
+        src={avatar_url}
+        alt=""
+        className="round-img"
+        style={{ width: "60px" }}
+      />
+      <h3>{login}</h3>
+      <div>
+        <a href={html_url} className="btn btn-dark btn-sm my-1">
+          More
+        </a>
       </div>
-    );
-  }
-}
+    </div>
+  );
+};
 
 export default UserItem;
